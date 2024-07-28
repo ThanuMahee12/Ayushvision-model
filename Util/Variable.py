f78da215d03d1f4c57d246cbbefccd86e690d9ac
yukatharsana <jeeyukatharsa@gmail.com>
2024-07-28 10:28:11 +0530

    Classification Dataset Prepared

--- Util/Variable.py
+++ Util/Variable.py
@@ -8,6 +8,7 @@ summary_report_dir=os.environ.get('SUMMARY_DICTORY')
 working_dirctory=os.getenv("TARGET_DATASET_PATH")
 working_dir_name=os.getenv('WORKING_DIR_NAME')
 firebaseKey=os.environ.get('FIREBASEKEY')
+ready_dir_name=os.environ.get('PREPARE_DIR_NAME')
 
 #
 # ayush_vision_json=
@@ -16,10 +17,9 @@ todaysummaryKey=today.strftime('%Y_%m_%d')
 today_summary_path=os.path.join(summary_report_dir,today.strftime('%Y_%m_%d'))
 today_analysis_path=os.path.join(today_summary_path,'analysis') # from Variables
 today_classfication_analysis_path=os.path.join(today_summary_path,'analysis','classfication') # from Variables
-today_classfication_non_eligble_csv=os.path.join(today_classfication_analysis_path,'classification_non_elible.csv')
 
 ayush_classFication_Working=os.path.join(working_dirctory,working_dir_name,'classification')
 ayush_detection_Working=os.path.join(working_dirctory,working_dir_name,'detection')
-
+ayush_classFication_Working_ready=os.path.join(working_dirctory,working_dir_name,ready_dir_name)
 today_collection_summary_path=os.path.join(today_summary_path,'collection')
 ayush_classification_prepare_summary=os.path.join(today_summary_path,'prepare','classfication') # from Variables
